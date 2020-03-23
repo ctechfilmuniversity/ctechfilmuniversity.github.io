a40a2a53e37ca43e94d0202c1a55d3e67506c1d3
Markus Traber <markus@fuchsteufels.de>
2020-03-23 16:27:31 +0100

    Test

--- __scripts/update_posts.py
+++ __scripts/update_posts.py
@@ -1,15 +1,41 @@
-import sys;
+import sys
+import os
 
-from os import walk
+path_new_post = "__content/__new-post/"
+path_existing_posts = "__content/"
 
 
-print("Starting to look for new or updated content")
+print("### Starting to look for new or updated content")
+check_for_new_or_updated_posts()
 
+def check_for_new_or_updated_posts():
+    new_post_files = index_files_folders(true, path_new_post)
+    #existing_posts = index_files_folders(true, path_existing_posts)
 
-f = []
-for (dirpath, dirnames, filenames) in walk("__content/"):
-    print(dirpath)
-    print(dirnames)
-    print(filenames)
-    f.extend(filenames)
-    break
+    if len(new_post_files) > 0:
+        create_new_post(new_post_files)
+    
+
+def index_files_folders(return_files, path):
+    files = []
+    folders = []
+    for (_, dirnames, filenames) in walk(path):
+        folders.extend(dirnames)
+        files.extend(filenames)
+        break
+    if return_files:
+        return files
+    else:
+        return folders
+
+def create_new_post(files):
+    path = path_existing_posts + "test_folder"
+    try:
+        os.mkdir(path)
+    except OSError:
+        print ("Creation of the directory %s failed" % path)
+    else:
+        print ("Successfully created the directory %s " % path)
+
+    print("####### test folder erstellt?")
+    index_files_folders(path_existing_posts)
