3b0d91f1c5cd7a0f9692a4f994cc93b24443e451
Markus Traber <markus@fuchsteufels.de>
2020-03-23 12:53:06 +0100

    Test

--- __scripts/update_posts.py
+++ __scripts/update_posts.py
@@ -3,11 +3,13 @@ import sys;
 from os import walk
 
 
-print(sys.version)
+print("Starting to look for new or updated content")
 
 
 f = []
 for (dirpath, dirnames, filenames) in walk("__content/"):
+    print(dirpath)
+    print(dirnames)
     print(filenames)
     f.extend(filenames)
     break
