a731ccca33ef9e4413987c6d1421861dbd4cc3cd
Markus Traber <markus@fuchsteufels.de>
2020-03-23 12:44:29 +0100

    Test

--- __content/update_posts.py
+++ __content/update_posts.py
@@ -1,3 +1,13 @@
-import sys; 
+import sys;
+
+from os import walk
+
 
 print(sys.version)
+
+
+f = []
+for (dirpath, dirnames, filenames) in walk(mypath):
+    print(filenames)
+    f.extend(filenames)
+    break
